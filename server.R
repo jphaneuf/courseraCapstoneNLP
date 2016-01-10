7fd7227c8d68cca9f85d807494fc5e32ac9a1b7a
Joe Phaneuf <phaneuf.joe@gmail.com>
2016-01-09 17:51:05 -0800

    shiny app with up-to quadgram prediction working

--- server.R
+++ server.R
@@ -1,32 +1,33 @@
 library(shiny)
 bigrams <- as.matrix(read.csv('bigrams.csv',row.names=1))
+trigrams <- as.matrix(read.csv('trigrams.csv',row.names=1))
+quadgrams <- as.matrix(read.csv('quadgrams.csv',row.names=1))
+findNgramInData <- function(ngram,dtm) head(grep(ngram,rownames(dtm),value=TRUE),n=1)
+
+predict <- function(inputString,nGramIn=3,nGramOut=4){
+  userInput <- tolower(inputString)
+  userInput <- paste(tail(unlist(strsplit(userInput,split=' ')),n=nGramIn),collapse="_")#last n tokens
+  userInput <- paste0('^',userInput)#look at beginning of line
+  prediction <- switch(as.character(nGramOut),
+     '2'=findNgramInData(userInput,bigrams),
+     '3'=findNgramInData(userInput,trigrams),
+     '4'=findNgramInData(userInput,quadgrams))
+  prediction <- paste(unlist(strsplit(prediction,split='_'))[-(1:nGramIn)],collapse=" ")
+  prediction <- paste(inputString,prediction)
+  return(prediction)
+}
+
 shinyServer(function(input, output) {
   #output$predictedText <- input$inputText#renderText({
   output$predictedText <- renderText({
-    userInput <- tolower(input$inputText)
-    userInput <- gsub(' ','_',userInput)
-    userInput <- paste0('^',userInput)#look at beginning of line
-    
-    #qi <- head(grep(userInput,rownames(quadgrams)),n=1)
-    #ti<- head(grep(userInput,rownames(trigrams)),n=1)
-    bi<- head(grep(userInput,rownames(bigrams)),n=1)
-    if(length(bi)) bf<-bigrams[bi,'total'] else bf <- 0#frequency
-    #if(length(ti)) tf<-trigrams[ti,'total'] else tf <- 0#frequency
-    #if(length(qi)) qf<-quadgrams[qi,'total'] else qf <- 0#frequency
-    #print(bf)
-    if(bf>0){
-      predicted <- rownames(bigrams)[bi]
+    nTokens <- length(unlist(strsplit(input$inputText,split=' ')))
+    #assume predicting a quadgram with a trigram input is the most accurate, and work backwords from there
+    for (i in min(nTokens,3):1){
+      for(j in 4:2){
+        result <- predict(input$inputText,nGramIn=i,nGramOut=j) 
+        if(length(result)) return(result)
+        
+      }
     }
-    else{
-      predicted <- userInput
-    }
-      
-    #print(tf)
-    #if(tf>0) print(rownames(trigrams)[ti])
-    #print(qf)
-    #if(qf>0) print(rownames(quadgrams)[qi])
-    #paste0(input$inputText,' fuck you')
-    predicted <- gsub('_',' ',predicted) 
-    predicted
   })
 })
